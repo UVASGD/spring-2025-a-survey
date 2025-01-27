bbbde158c94761c82ee52740139f1c11e81cde28
Mush <greenfishfungus2@gmail.com>
2025-01-27 18:18:11 -0500

    first commit

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Testing.ArcheryKit
+{
+    public class Destructible : ArrowTargetBase
+    {
+        public GameObject destructedObject;
+
+        protected override void _OnHit(ArrowProp arrow)
+        {
+            base._OnHit(arrow);
+            GameObject destructed = Instantiate(destructedObject, transform.position, transform.rotation);
+
+            var rbs = destructed.GetComponentsInChildren<Rigidbody>();
+            foreach (var rb in rbs)
+            {
+                rb.AddExplosionForce(1000f, transform.position, 5);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/StationTest.cs
@@ -0,0 +1,46 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.Economy;
+using VRC.SDK3.ClientSim;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class StationTest : UdonSharpBehaviour
+{
+    private Vector3 startPosition;
+    private float timer = 0f;
+    private float moveSpeed = 1f; // Adjust this to control movement speed
+    private float moveDistance = 3f; // Distance to move left/right
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+    public override void Interact()
+    {
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        Debug.Log($"{player.displayName} Entered");
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        Debug.Log($"{player.displayName} Exited");
+    }
+
+    private void Update()
+    {
+        // Increment timer
+        timer += Time.deltaTime * moveSpeed;
+
+        // Calculate the new x position using a sine wave for smooth back-and-forth movement
+        float newX = startPosition.x + Mathf.Sin(timer) * moveDistance;
+
+        // Update position
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z);
+        transform.position = newPosition;
+    }
+}
